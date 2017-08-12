0b851ed418499bb77407dd31e0e537c97419a525
djo <declan.l.johnson@gmail.com>
2017-08-11 23:01:03 -0500

    dapp init Backend

---
+++ src/Backend.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.13;
+
+import "ds-test/test.sol";
+
+import "./Backend.sol";
+
+contract BackendTest is DSTest {
+    Backend backend;
+
+    function setUp() {
+        backend = new Backend();
+    }
+
+    function testFail_basic_sanity() {
+        assert(false);
+    }
+
+    function test_basic_sanity() {
+        assert(true);
+    }
+}
