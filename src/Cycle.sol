056a00ecc0cb3adac2f5013551e59297f562399d
djo <declan.l.johnson@gmail.com>
2017-09-21 16:45:58 -0500

    Update total supply to constant

--- src/Cycle.sol
+++ src/Cycle.sol
@@ -24,7 +24,7 @@ contract token {
 	string public constant name = 'codename cycle';
 	string public constant symbol = 'ccy';
 	uint8 public constant decimals = 18;
-	uint256 public totalSupply;
+	uint256 public constant totalSupply = 0xffffffff;
 
 	/* This creates an array with all balances */
 	mapping (address => uint256) public balanceOf;
@@ -36,16 +36,15 @@ contract token {
 	/* Initializes contract with initial supply tokens to the creator of the contract */
 	function token(uint256 initialSupply) {
 		balanceOf[msg.sender] = initialSupply;	// Give the creator all initial tokens
-		totalSupply = initialSupply;		// Update total supply
 	}
 
 	/* Send coins */
 	function transfer(address _to, uint256 _value) {
-		require(balanceOf[msg.sender] > _value);           // Check if the sender has enough
-		require(balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
-		balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-		balanceOf[_to] += _value;                            // Add the same to the recipient
-		Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+		require(balanceOf[msg.sender] > _value);           
+		require(balanceOf[_to] + _value > balanceOf[_to]); 
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+		Transfer(msg.sender, _to, _value);
 	}
 
 
