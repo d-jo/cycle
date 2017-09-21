f0fb1a4a47a4012687f46f777568f84bea8e83ae
djo <declan.l.johnson@gmail.com>
2017-09-21 18:02:41 -0500

    Add missing ERC20 function, documentation

--- src/Cycle.sol
+++ src/Cycle.sol
@@ -17,17 +17,25 @@ contract owned {
 	}
 }
 
-/*
-
+/**
+	spec for the ERC20 interface, a standard for Ethereum tokens.
 */
 contract ERC20Interface {
+	// get the total supply of the token
 	function totalSupply() constant returns (uint256 totalSupply);
+	// get the account balance of _owner
 	function balanceOf(address _owner) constant returns (uint256 balance);
+	// transfer _value tokens to address _to
 	function transfer(address _to, uint256 _value) returns (bool success);
+	// transfer _value tokens from address _from to address _to
 	function transferFrom(address _from, address _to, uint256 _amount) returns (bool success);
+	// allow _spender to spend _value tokens from msg.senders account
 	function approve(address _spender, uint256 _value) returns (bool success);
+	// returns the amount of tokens _spender has left
 	function allowance(address _owner, address _spender) constant returns (uint remaining);
+	// event triggered when tokens are transfered
 	event Transfer(address indexed _from, address indexed _to, uint _value);
+	// event triggered when approve is called
 	event Approval(address indexed _owner, address indexed _spender, uint _value);
 }
 
@@ -48,14 +56,21 @@ contract AIToken is owned, ERC20Interface {
 	mapping (address => uint256) public balances;
 	mapping (address => mapping (address => uint256)) public allowed;
 
+	// constructor for token, assign all tokens to creator
 	function AIToken() {
 		balances[msg.sender] = _totalSupply;
 	}
 
+	function totalSupply() constant returns (uint256 totalSupply) {
+		totalSupply = _totalSupply;
+	}
+	
+	// implementation of ERC20 balance function
 	function balanceOf(address _owner) constant returns (uint256 balance) {
-		return balances[_owner];
+		return balances[_owner]; // return the balance of the address requested
 	}
 
+	// implementation of ERC20 transfer function
 	function transfer(address _to, uint256 _value) returns (bool success) {
 		if((balances[msg.sender] > _value) && (balances[_to] + _value > balances[_to])) {
 			balances[msg.sender] -= _value;
@@ -68,6 +83,7 @@ contract AIToken is owned, ERC20Interface {
 	}
 
 
+	// implementation of ERC20 transferFrom function
 	function transferFrom(address _from, address _to, uint256 _amount) returns (bool success) {
 		if(
 			(balances[_from] > _amount) && 
@@ -83,12 +99,14 @@ contract AIToken is owned, ERC20Interface {
 		return false;
 	}
 
+	// implementation of ERC20 approve function
 	function approve(address _spender, uint256 _amount) returns (bool success) {
 		allowed[msg.sender][_spender] = _amount;
 		Approval(msg.sender, _spender, _amount);
 		return true;
 	}
 
+	// implementation of ERC20 allowance function
 	function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
 		return allowed[_owner][_spender];
 	}
